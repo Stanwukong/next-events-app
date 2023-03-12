ad31458a85e7315179a70cc66c1e4735562bb458
WuKong <111810384+Stanwukong@users.noreply.github.com>
2023-03-12 16:23:02 +0100

    Added general layout wrapper component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import MainHeader from "../components/layout/main-header"
+import "../styles/globals.css"
+import { Fragment } from "react"
+import Layout from "../components/layout/layout"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
